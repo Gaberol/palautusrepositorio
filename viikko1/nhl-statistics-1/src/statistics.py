c582a918132ce5f262140e970eabd27eeb93be0d
Kaapo Kivioja <kaapo.kivioja@helsinki.fi>
2022-11-07 21:01:55 +0200

    teht. 15

--- viikko1/nhl-statistics-1/src/statistics.py
+++ viikko1/nhl-statistics-1/src/statistics.py
@@ -8,7 +8,6 @@ def sort_by_points(player):
 class Statistics:
     def __init__(self, reader):
         self.reader = reader
-
         self._players = reader.get_players()
 
     def search(self, name):
